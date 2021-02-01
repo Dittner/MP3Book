559462f669ccb79439f1b7a8a9681784f637cc64
Alexander Dittner <alexander.dittner@gmail.com>
2021-02-01 23:17:03 +0300

    Initial Commit

---
+++ MP3Book/MP3BookApp.swift
@@ -0,0 +1,17 @@
+//
+//  MP3BookApp.swift
+//  MP3Book
+//
+//  Created by Alexander Dittner on 01.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct MP3BookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
