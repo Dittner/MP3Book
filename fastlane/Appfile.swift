e6ce4c69d5b69444140771aa17e4526e1251799c
Alexander Dittner <alexander.dittner@gmail.com>
2021-04-24 16:46:28 +0300

    delete

--- fastlane/Appfile.swift
+++
@@ -1,9 +0,0 @@
-var appIdentifier: String { return "de.dittner.mp3book" } // The bundle identifier of your app
-var appleID: String { return "alexander.dittner@icloud.com" } // Your Apple email address
-
-var itcTeam: String? { return "120174276" } // App Store Connect Team ID
-var teamID: String { return "F3QYFD9Z4F" } // Apple Developer Portal Team ID
-
-
-// For more information about the Appfile, see:
-//     https://docs.fastlane.tools/advanced/#appfile
